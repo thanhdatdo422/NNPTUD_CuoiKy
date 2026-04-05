aff807117990d5896c0c07c346f84d712a7ecf98
anhminh2307 <anhminh23072004@gmail.com>
2026-04-05 23:21:09 +0700

    fix not author as admin

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -11,7 +11,7 @@ const protect = async (req, res, next) => {
     try {
       token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET || 'secret');
-      req.user = await User.findById(decoded.id).select('-password');
+      req.user = await User.findById(decoded.id).select('-password').populate('role');
       next();
     } catch (error) {
       res.status(401).json({ message: 'Not authorized, token failed' });
